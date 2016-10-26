9cf52fff9d7cccbd0cef7f98ac47a5f09325f554
Chao Wei Chiu <chaoweichiu@CW-2.local>
2016-10-26 16:07:22 -0400

    added getIndexToIns

--- test/getIndexToIns-spec.js
+++ test/getIndexToIns-spec.js
@@ -1,45 +1,42 @@
 const expect = require('chai').expect
 
-var speak = function(things, d){
-	console.log(things)
+
+function getIndexToIns(arr, goal) {
+	var sortedArray = arr.sort(function(a,b){return a-b})
 	
-	console.log(this)
+	if(sortedArray.indexOf(goal)!==-1)return sortedArray.indexOf(goal)
+	
+	if(sortedArray[sortedArray.length-1] < goal) return sortedArray.length
+	
+	var indexForTarget =  sortedArray.reduce(function(prev, curr){
+  		return (Math.abs(curr - goal) <= Math.abs(prev - goal) ? curr : prev)
+	})
+	return sortedArray.indexOf(indexForTarget)
+
+
 }
-var sayThings = {name: 'CW', age: 22}
-var sayThing = {height:177}
-speak.call(sayThings, ['dd', 'gg'])
 
-// function getIndexToIns(arr, goal) {
-// 	console.log(arguments)
-// 	let sortedArray = arr.sort(function(a,b){return a-b})
+getIndexToIns([40, 60], 50);
 
 
-//  	if(sortedArray.indexOf(goal) !== -1)return sortedArray.indexOf(goal)
+describe('getIndexToIns()', ()=>{
+	it('(1) One of nums in array matchs target ', ()=>{
+		expect(getIndexToIns([10, 20, 30, 40, 50], 30)).to.equal(2)
+	})
+	it('(2) Find the closet num', ()=>{
+		expect(getIndexToIns([2, 20, 10], 19)).to.equal(2)
+		
+	})
+	it('(3) return bigger number if the distance from two number are the same', ()=>{
+		expect(getIndexToIns([40, 60], 50)).to.equal(1)
+	})
+	it('(4) goal is bigger than any number in array', ()=>{
+		expect(getIndexToIns([2, 5, 10], 15)).to.equal(3)
+	})
+})
 
-//  	var n = sortedArray.reduce(function (prev, curr) {
-//   		return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev)
-// 	})
-	
-// 	console.log(n)
-// 	return sortedArray.indexOf(n) + 1
 
 
-// }
-
-// getIndexToIns([40, 60], 50);
 
 
-// describe('getIndexToIns()', ()=>{
-// 	it('(1)', ()=>{
-// 		expect(getIndexToIns([10, 20, 30, 40, 50], 35)).to.equal(3)
-// 		expect(getIndexToIns([40, 60], 50)).to.equal(1)
-// 		expect(getIndexToIns([3, 10, 5], 3)).to.equal(0)
-// 	})
-// 	it.only('(2)', ()=>{
-// 		expect(getIndexToIns([10, 20, 30, 40, 50], 30)).to.equal(2)
-// 		expect(getIndexToIns([2, 20, 10], 19)).to.equal(2)
-// 	})
-// 	it('(3)', ()=>{
-// 		expect(getIndexToIns([5, 3, 20, 3], 5)).to.equal(2)
-// 	})
-// })
+
