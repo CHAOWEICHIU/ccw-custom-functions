2fc22fa504d0dedb4147ed1ce1178e1654979765
Chao Wei Chiu <chaoweichiu@CW-2.local>
2016-11-02 10:37:52 -0400

    added truncateString

--- test/units/truncateString-spec.js
+++ test/units/truncateString-spec.js
@@ -1,17 +1,9 @@
 const expect = require('chai').expect
+	, truncateString = require('../../lib/custom_functions/truncateString')
 
 
 
-function truncateString(str, num) {
-  	
-  	if(str.length === num) return str
-	if(str.length < num) return str
-  	var cutIndex = num < 3 ? num : num - 3
-  
-  	var result = str.substring(0, cutIndex)
-  
-  return result + '...'
-}
+
 
 
 describe('truncateString()', ()=>{
