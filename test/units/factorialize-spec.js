e76d15eb012270079e6b5e99aaef5be125e14751
Chao Wei Chiu <chaoweichiu@CW-2.local>
2016-11-02 10:33:40 -0400

    added findLongestWord

--- test/units/factorialize-spec.js
+++ test/units/factorialize-spec.js
@@ -8,7 +8,6 @@ describe('factorialize', ()=>{
 		expect(factorialize(10)).to.equal(3628800)
 		expect(factorialize(20)).to.equal(2432902008176640000)
 		expect(factorialize(0)).to.equal(1)
-		
 	})
 })
 
