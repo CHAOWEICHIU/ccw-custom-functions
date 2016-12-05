8c23fe429a3f6d35646a41ebd8bb4c3da9e912b2
chao wei chiu <chaoweichiu@gmail.com>
2016-12-05 09:27:00 -0500

    adde flattenArray

---
+++ test/units/flattenArray.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect
+const flattenArray = require('../../lib/custom_functions/flattenArray')
+
+describe('array', ()=>{
+  let input, output
+  beforeEach(()=>{
+    input = ''
+    output = ''
+  })
+  it('test (1)', ()=>{
+    input = [1,2,3,[4]]
+    output = [1,2,3,4]
+    expect(flattenArray(input)).to.eql(output)
+  })
+  it('test (2)', ()=>{
+    input = [[1,2,[3, [4]],[5,[5,1,5,[4]],5]],4]
+    output = [ 1, 2, 3, 4, 5, 5, 1, 5, 4, 5, 4 ]
+    expect(flattenArray(input)).to.eql(output)
+  })
+})
