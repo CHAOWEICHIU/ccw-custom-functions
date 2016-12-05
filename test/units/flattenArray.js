f52bf728a6e0a2ba92630a65f095706f2662faab
chao wei chiu <chaoweichiu@gmail.com>
2016-12-05 09:55:30 -0500

    added one more test case to flattenArray

--- test/units/flattenArray.js
+++ test/units/flattenArray.js
@@ -17,4 +17,9 @@ describe('array', ()=>{
     output = [ 1, 2, 3, 4, 5, 5, 1, 5, 4, 5, 4 ]
     expect(flattenArray(input)).to.eql(output)
   })
+  it('test (3)', ()=>{
+    input = [[[0], [1]], [[2], [3]], [[4], [5]]]
+    output = [0,1,2,3,4,5]
+    expect(flattenArray(input)).to.eql(output)
+  })
 })
